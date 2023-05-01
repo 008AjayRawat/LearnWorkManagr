ecbdd09238ec5e89acbd75a11a24719294630be8
008AjayRawat <ajayrawat.android@gmail.com>
2023-05-01 16:04:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/learn/learnworkmanager/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.learn.learnworkmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.learn.learnworkmanager.ui.theme.LearnWorkManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearnWorkManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
